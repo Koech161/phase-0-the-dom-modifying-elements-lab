0d4f6ef9c81536654dd636dc711e15f8801c9fde
Abel Koech <koechabel161@gmail.com>
2024-06-23 17:45:42 +0300

    elements-lab,

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove("main");
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
